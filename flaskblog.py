92dbb096e35bf86f09dcd90432ddc78ee09a8489
= <=>
2019-02-19 19:54:42 +0530

    forms and validation

--- flaskblog.py
+++ flaskblog.py
@@ -6,8 +6,12 @@ Created on Sun Sep 16 16:47:10 2018
 @author: raunak
 """
 
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '5bcc0f9e34c15efa9fefb136c16730ec'
 posts = [
             {
                 'author':'Raunak Sharma',
@@ -25,12 +29,26 @@ posts = [
         ]
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
     return render_template('About.html',title="about")
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("Account created for {{form.username.data}}!","success")
+        return redirect(url_for('home'))
+    return render_template('register.html',title="Register",form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title="Login",form=form)
+    
 
 
 if __name__ == "__main__":
