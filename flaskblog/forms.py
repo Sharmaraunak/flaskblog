0855d59ed21e40bfe2bdbc78eaff07e24bc5f0ba
Raunak <sharmaraunak338@gmail.com>
2019-08-27 17:41:57 +0530

    account image updation

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flaskblog.models import User
@@ -35,7 +36,30 @@ class LoginForm(FlaskForm):
 	remember = BooleanField("Remember Me")
 	submit = SubmitField("Login")
 
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField("Username",
+		validators = [DataRequired(),Length(min = 2,max = 10)])
+	email = StringField("Email",
+		validators = [DataRequired(),Email()])
 	
+	update = SubmitField("Update")
+
+
+	def validate_username(self,username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username = username.data).first()
+
+			if user:
+				raise ValidationError('That username is taken .Please choose a different one.')
+
+
+	def validate_email(self,email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email = email.data).first()
+
+			if user:
+				raise ValidationError('That email is taken .Please choose a different one.')
 
 
 
