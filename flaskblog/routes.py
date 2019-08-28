ea80d03e4d0924a53db2a877df7bc24ee71b7869
Raunak <sharmaraunak338@gmail.com>
2019-08-29 04:03:18 +0530

    pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -10,7 +10,8 @@ from flask_login import login_user,current_user,logout_user,login_required
 
 @app.route("/")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page',1,type = int)
+    posts = Post.query.paginate(page = page,per_page = 5)
     return render_template('home.html',posts = posts)
 
 @app.route("/about")
